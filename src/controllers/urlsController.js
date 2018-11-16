f511ffb3091d6c04f57d5dfd2ddbfceaea8fce5b
majesnix <dominic@dominic-classen.de>
2018-11-16 01:05:45 +0100

    return error when invalid Uri

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -6,23 +6,25 @@ const { Urls } = require('../db');
 const { isUri } = require('valid-url');
 const shortid = require('shortid');
 
-
 exports.create_url = async (req, res) => {
 	const { url } = req.body;
 
 	// create and save shortlink to database
 	try {
-
-		const id = shortid.generate();
 		if (isUri(url)) {
+			const id = shortid.generate();
 			await Urls.create({
 				short_url: id,
 				long_url: url,
 			});
-	
+
 			return res.status(200).json({
 				base: url,
-				short: id
+				short: id,
+			});
+		} else {
+			return res.status(400).json({
+				message: 'Not a valid url',
 			});
 		}
 	} catch (err) {
@@ -40,13 +42,13 @@ exports.get_url = async (req, res) => {
 		const urlObj = await Urls.findOne({ short_url });
 		if (urlObj) {
 			return res.status(200).json({
-				url: urlObj.long_url
+				url: urlObj.long_url,
 			});
 		}
 	} catch (err) {
 		req.log.error(err);
 		return res.status(404).json({
-			message: 'ID not found'
+			message: 'ID not found',
 		});
 	}
 };
