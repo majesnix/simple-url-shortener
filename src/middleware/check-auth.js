44932f59e9a6d904411354c7a7e7fd0e45a800a4
majesnix <dominic@dominic-classen.de>
2018-11-18 09:49:20 +0100

    remove decoded user, because i dont need this

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1];
-		const decoded = jwt.verify(token, process.env.JWT_KEY);
-		req.user = decoded;
+		jwt.verify(token, process.env.JWT_KEY);
+
 		next();
 	} catch (err) {
 		console.log(err);
