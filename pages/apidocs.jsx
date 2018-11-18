4d7a70861d1363554f2969c42dd516b1f705e56a
majesnix <dominic@dominic-classen.de>
2018-11-18 14:54:14 +0100

    some tweaking, and that should be enough

--- pages/apidocs.jsx
+++ pages/apidocs.jsx
@@ -11,29 +11,42 @@ const Api = () => (
 				justifyContent: 'center',
 				alignItems: 'center',
 				height: '100%',
-				marginTop: '15px'
+				marginTop: '15px',
 			}}
 		>
 			<h1>API Documentation</h1>
-			<div style={{ width: '50%', fontSize: '2rem', marginTop: 20 }}>
+			<div style={{ width: '50%', fontSize: '1.5rem', marginTop: 20 }}>
 				Yes, you can use our service with an API.
-				<br/><br/>
+				<br />
+				<br />
 				<table>
 					<thead>
-						<th>Method</th>
-						<th>Endpoint</th>
-						<th>Data</th>
-						<th>Returns</th>
+						<th>method</th>
+						<th>endpoint</th>
+						<th>req.query</th>
+						<th>req.body</th>
 					</thead>
 					<tbody>
 						<tr>
+							<td>GET</td>
+							<td>/api/</td>
+							<td>url: url to shorten</td>
+							<td />
+						</tr>
+						<tr>
 							<td>POST</td>
 							<td>/api/</td>
-							<td>BODY url</td>
-							<td>JSON short_url, long_url</td>
+							<td></td>
+							<td>url: url to shorten</td>
 						</tr>
 					</tbody>
 				</table>
+				<br/><br/>
+				Both return a JSON Object with two parameters:
+				<br/><br/>
+				<strong>base:</strong> the url you send to us
+				<br/>
+				<strong>short:</strong> your generated short-url
 			</div>
 		</div>
 	</div>
