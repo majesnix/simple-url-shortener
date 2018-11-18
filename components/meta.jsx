0d0153720cfee126b4574ec3bef493bf56636af4
majesnix <dominic@dominic-classen.de>
2018-11-18 12:55:16 +0100

    add meta tags + safari-pinned-tag image

--- components/meta.jsx
+++ components/meta.jsx
@@ -87,6 +87,70 @@ export default () => (
 				content="/static/ms-icon-144x144.png"
 			/>
 			<meta name="theme-color" content="#ffffff" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/static/apple-touch-icon.png?v=XBreOJMe24"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				href="/static/favicon-32x32.png?v=XBreOJMe24"
+				sizes="32x32"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				href="/static/favicon-16x16.png?v=XBreOJMe24"
+				sizes="16x16"
+			/>
+			<link
+				rel="mask-icon"
+				href="/static/safari-pinned-tab.svg?v=XBreOJMe24"
+				color="#5bbad5"
+			/>
+			<link
+				rel="shortcut icon"
+				href="/static/favicon.ico?v=XBreOJMe24"
+			/>
+			<meta name="apple-mobile-web-app-title" content="dcl.re" />
+			<meta name="application-name" content="dcl.re" />
+			<meta
+				name="msapplication-config"
+				content="/static/browserconfig.xml?v=XBreOJMe24"
+			/>
+			<meta name="theme-color" content="#ffffff" />
+
+			<meta property="og:url" content="https://dcl.re" />
+			<meta property="og:type" content="website" />
+			<meta
+				property="og:title"
+				content="dcl.re | makes links short."
+			/>
+			<meta property="og:description" content="simple linkshortener" />
+			<meta
+				property="og:image"
+				content="/static/logo_square.png"
+			/>
+			<meta
+				property="og:image:secure_url"
+				content="/static/logo_square.png"
+			/>
+
+			<meta name="twitter:card" content="summary" />
+			<meta
+				name="twitter:title"
+				content="dcl.re | makes links short."
+			/>
+			<meta name="twitter:description" content="simple linkshortener" />
+			<meta
+				name="twitter:image"
+				content="/static/logo_square.png"
+			/>
+			<meta
+				name="twitter:image:src"
+				content="/static/logo_square.png"
+			/>
 		</Head>
 		<style jsx global>{`
 			body {
